19f74d90d7c3328d887fe101722c531975494163
Mariia <>
2022-06-04 23:14:37 +0300

    решение задачи 41

---
+++ Tasks/Task_41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.WriteLine("Введите любое количество чисел через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+        if(array[i] > 0)
+    {
+      count++;  
+    }
+}
+Console.WriteLine($"Количество положительных элементов массива: {count}");
